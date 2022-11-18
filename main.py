159bd3057127bd1b0cd8385023e5be7266e4e5ea
Rx584 <xiao_2010@outlook.com>
2022-11-18 19:37:03 +0800

    Add output

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from PIL import Image
-origin = input("原图片路径")
-output_name = input("输出文件名称")
-img = Image.open(origin).convert("L").resize((200,200))
+
+img = Image.open("origin.png").convert("L").resize((200,200))
 width = img.width
 height = img.height
 ASCII_HIGH = '''$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. '''
@@ -13,5 +12,5 @@ for y in range(width):
         index=int(gray/256*70)
         output += ASCII_HIGH[index]+" "
     output += "\n"
-with open(output_name,mode = "w",encoding="utf-8")as f:
+with open("output.txt",mode = "w",encoding="utf-8")as f:
     f.write(output)
